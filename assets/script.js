a631836c5b6e3681883c72ac5b608a2965b0d5fb
Hieu Tran <hieuthanhtran98@yahoo.com>
2023-03-22 17:29:21 -0700

    added the current time

--- assets/script.js
+++ assets/script.js
@@ -20,4 +20,13 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  
+  //we want MM DD YYYY
+    const currentDay = dayjs().format(" MM, DD YYYY");
+    const currentTime = dayjs().format(" h:mm:A"); 
+    $('header').append(currentDay);
+    
+   $('header').append(currentTime);
+
+
 });
