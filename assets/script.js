963fabab6507237436e690d320ac87a96d1156c2
Hieu Tran <hieuthanhtran98@yahoo.com>
2023-03-24 16:40:46 -0700

    we added the function to save to our local storage

--- assets/script.js
+++ assets/script.js
@@ -2,12 +2,42 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+  //save functionality in class 
+  $(".saveBtn").on("click", function(){
+    // console.log("save button is working");
+    //save the user informaton
+    //getting the text out of our text area, made it jquery by putting the $ in front 
+    const val =$(this).siblings("textarea").eq(0).val();
+    //were targeting the parent id so that we can then use this information to figure out the past,present, and future
+    const id = $(this).parent().attr("id");
+    //this is how we save the line of code 
+    localStorage.setItem(id, val);
+    console.log(id,val);
+  });
+    
+//creating a time block function to let us know where were saving it to 
+  $(".time-block").each(function (){
+    //setting up conditional statements for past present and future css 
+    const id = $(this).attr("id");
+    const val = localStorage.getItem(id);
+    $(this).children("textarea").eq(0).val(val);
+  });
+
+
+  });
+
+  //making the save functionality 
+  // const saveButtons = document.querySelectorAll('.saveBtn');
+  // saveButtons.forEach(saveButton => {
+  //   saveButton.addEventListener('click', function(){
+  //   const timeBlock = saveButton.closest('.timeblock');
+  //   const textInput = timeBlock.querySelector('.description').value;
+
+  //   //getting the id of the time block and use it as the key to save the input 
+  //   const timeBlockId = timeBlock.id;
+  //   localStorage.setItem(timeBlockId, textInput);
+  //   })
+  
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
@@ -29,4 +59,4 @@ $(function () {
    $('header').append(currentTime);
 
 
-});
+
